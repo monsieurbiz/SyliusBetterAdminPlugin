2e5ee074f20e199d5bf5b6a86a49d0b287cafc06
Maxime Huran <m.huran@monsieurbiz.com>
2024-03-16 16:54:39 +0100

    Manage custom colors and CSS in admin

--- src/Form/Type/Settings/AdminSettingsType.php
+++ src/Form/Type/Settings/AdminSettingsType.php
@@ -15,6 +15,7 @@ use MonsieurBiz\SyliusMediaManagerPlugin\Form\Type\ImageType;
 use MonsieurBiz\SyliusSettingsPlugin\Form\AbstractSettingsType;
 use MonsieurBiz\SyliusSettingsPlugin\Form\SettingsTypeInterface;
 use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class AdminSettingsType extends AbstractSettingsType implements SettingsTypeInterface
@@ -36,6 +37,16 @@ class AdminSettingsType extends AbstractSettingsType implements SettingsTypeInte
         );
         $this->addWithDefaultCheckbox(
             $builder,
+            'circular_icon_color',
+            ColorType::class,
+            [
+                'label' => 'monsieurbiz_better_admin.form.circular_icon_color',
+                'help' => 'monsieurbiz_better_admin.form.circular_icon_color_help',
+                'required' => false,
+            ]
+        );
+        $this->addWithDefaultCheckbox(
+            $builder,
             'logo',
             ImageType::class,
             [
@@ -51,6 +62,16 @@ class AdminSettingsType extends AbstractSettingsType implements SettingsTypeInte
             [
                 'label' => 'monsieurbiz_better_admin.form.logo_dark',
                 'help' => 'monsieurbiz_better_admin.form.logo_dark_help',
+                'required' => false,
+            ]
+        );
+        $this->addWithDefaultCheckbox(
+            $builder,
+            'custom_css',
+            TextareaType::class,
+            [
+                'label' => 'monsieurbiz_better_admin.form.custom_css',
+                'help' => 'monsieurbiz_better_admin.form.custom_css_help',
                 'required' => false,
             ]
         );
---
+++ src/Twig/Extension/ColorExtension.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz's  for Sylius.
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusBetterAdminPlugin\Twig\Extension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+final class ColorExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('monsieurbiz_better_admin_hex_to_rgb', [$this, 'hexToRgb']),
+            new TwigFilter('monsieurbiz_better_admin_rgb_to_hex', [$this, 'rgbToHex']),
+            new TwigFilter('monsieurbiz_better_admin_hex_to_rgba', [$this, 'hexToRgba']),
+        ];
+    }
+
+    /**
+     * @SuppressWarnings(PHPMD.ElseExpression)
+     */
+    public function hexToRgb(string $hex): array
+    {
+        $hex = str_replace('#', '', $hex);
+        if (3 === \strlen($hex)) {
+            $red = hexdec($hex[0] . $hex[0]);
+            $green = hexdec($hex[1] . $hex[1]);
+            $blue = hexdec($hex[2] . $hex[2]);
+        } else {
+            $red = hexdec($hex[0] . $hex[1]);
+            $green = hexdec($hex[2] . $hex[3]);
+            $blue = hexdec($hex[4] . $hex[5]);
+        }
+
+        return [$red, $green, $blue];
+    }
+
+    public function rgbToHex(int $red, int $green, int $blue): string
+    {
+        return sprintf('#%02x%02x%02x', $red, $green, $blue);
+    }
+
+    public function hexToRgba(string $hex, float $alpha): string
+    {
+        $rgb = $this->hexToRgb($hex);
+
+        return sprintf('rgba(%d, %d, %d, %s)', $rgb[0], $rgb[1], $rgb[2], $alpha);
+    }
+}
