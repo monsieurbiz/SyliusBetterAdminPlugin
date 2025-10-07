232649faa283201b8f5961f771ed5ce47fea05fc
Tom Bourti <t.bourti@monsieurbiz.com>
2025-10-07 10:28:03 +0200

    fix(phpcs): reword the plugin name on comments

--- dist/src/Context/Channel/RequestBased/HostnameAndPortBasedRequestResolver.php
+++ dist/src/Context/Channel/RequestBased/HostnameAndPortBasedRequestResolver.php
@@ -1,7 +1,8 @@
 <?php
 
 /*
- * This file is part of Monsieur Biz' Scripts plugin for Sylius.
+ * This file is part of Monsieur Biz' Better Admin plugin for Sylius.
+ *
  * (c) Monsieur Biz <sylius@monsieurbiz.com>
  *
  * For the full copyright and license information, please view the LICENSE.txt
